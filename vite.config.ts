751c5c61987329aa2443d0745f805a95536c3944
Raj Chaudhari <raj.p.chaudhary@gmail.com>
2025-01-24 20:09:47 -0500

    add redux toolkit and setup store

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/my-app"
+  base:"/my-app",
+  server:{
+    port:3000
+  }
 })
